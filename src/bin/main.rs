28eded5a71967c430cefcedf0f5617807e8b0a3b
Jim Hessin <jhessin@gmail.com>
2021-06-02 08:32:35 -0600

    Set up a gotham server basic

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,54 +1,40 @@
-use hello::ThreadPool;
-use std::fs;
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::thread;
-use std::time::Duration;
+//! A Hello world example application for working with Gotham
 
-fn main() {
-  let listener = if let Ok(listener) = TcpListener::bind("127.0.0.1:8080") {
-    listener
-  } else {
-    return;
-  };
-  let pool = ThreadPool::new(4);
+extern crate gotham;
 
-  for stream in listener.incoming() {
-    let stream = if let Ok(stream) = stream { stream } else { break };
-    pool.execute(|| {
-      handle_connection(stream);
-    });
-  }
+use gotham::state::State;
 
-  println!("Shutting down.");
+const HELLO_WORLD: &str = "Hello World!";
+
+/// Create a 'Handler' which is invoked when responding to a 'Request'.
+pub fn say_hello(state: State) -> (State, &'static str) {
+  (state, HELLO_WORLD)
 }
 
-fn handle_connection(mut stream: TcpStream) {
-  let mut buffer = [0; 1024];
-  stream.read(&mut buffer).unwrap();
+/// Start a server and call the 'Handler' we've defined above for each 'Request' we receive.
+pub fn main() {
+  let addr = "127.0.0.1:8080";
+  println!("Listening for requests at http://{}", addr);
+  gotham::start(addr, || Ok(say_hello))
+}
 
-  let get = b"GET / HTTP/1.1\r\n";
-  let sleep = b"GET /sleep HTTP/1.1\r\n";
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use gotham::{hyper::StatusCode, test::TestServer};
 
-  let (status_line, filename) = if buffer.starts_with(get) {
-    ("HTTP/1.1 200 OK", "hello.html")
-  } else if buffer.starts_with(sleep) {
-    thread::sleep(Duration::from_secs(5));
-    ("HTTP/1.1 200 OK", "hello.html")
-  } else {
-    ("HTTP/1.1 404 NOT FOUND", "404.html")
-  };
+  #[test]
+  fn receive_hello_world_response() {
+    let test_server = TestServer::new(|| Ok(say_hello)).unwrap();
+    let response = test_server
+      .client()
+      .get("http://localhost")
+      .perform()
+      .unwrap();
 
-  let contents = fs::read_to_string(filename).unwrap();
+    assert_eq!(response.status(), StatusCode::OK );
 
-  let response = format!(
-    "{}\r\nContent-Length: {}\r\n\r\n{}",
-    status_line,
-    contents.len(),
-    contents
-  );
-
-  stream.write(response.as_bytes()).unwrap();
-  stream.flush().unwrap();
+    let body = response.read_body().unwrap();
+    assert_eq!(&body[..], b"Hello World!");
+  }
 }
